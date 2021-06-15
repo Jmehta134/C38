adecb2d3952417a4ae90ba900359ca49b3bd2a8b
swajaydeep@gmail.com <6575@jay>
2021-06-15 15:03:51 +0530

    upload

--- js/Game.js
+++ js/Game.js
@@ -25,6 +25,12 @@ class Game {
       }
       form = new Form()
       form.display();
+      car1 = createSprite(displayWidth/5,displayHeight/4,40,40);
+      car2 = createSprite(displayWidth/5*2,displayHeight/4,40,40);
+      car3 = createSprite(displayWidth/5*3,displayHeight/4,40,40);
+      car4 = createSprite(displayWidth/5*4,displayHeight/4,40,40);
+      cars = [car1,car2,car3,car4];
+    
     }
   }
 
@@ -35,16 +41,30 @@ class Game {
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
-      var display_position = 130;
+      background(20);
+      //var display_position = 130;
+      var x = 0;
+      var y = 0;
+      var index = 0;
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
+        index = index+1;
+        x += 200;
+        y = displayHeight-allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y; 
+        if (index === player.index){
+          cars[index-1].shapeColor=rgb(255,0,0);
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y;
+          /*
+          fill("red");
         else
           fill("black");
 
         display_position+=20;
         textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)*/
+        }
       }
     }
 
@@ -52,5 +72,6 @@ class Game {
       player.distance +=50
       player.update();
     }
+    drawSprites();
   }
-}
+}    
